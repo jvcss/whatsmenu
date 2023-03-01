12afcf674823f892171972df86f15fa0b0d49b60
João Victor <jvcs.mecatronica@gmail.com>
2023-03-01 11:51:45 -0300

    add MUI-5 theme

---
+++ theme/index.tsx
@@ -0,0 +1,18 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#3f51b5',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+  },
+  typography: {
+    fontFamily: 'Roboto, sans-serif',
+    fontSize: 16,
+  },
+});
+
+  export default theme;
